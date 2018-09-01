891912852768853c97eb845533e45b84d0756e12
Kevin Horng <tkevin.horng@gmail.com>
2018-09-02 09:43:04 +1000

    edited addRecall

--- src/components/addRecall/addRecall.js
+++ src/components/addRecall/addRecall.js
@@ -1,20 +1,138 @@
 import React, { Component } from 'react';
-
 import './addRecall.css';
 
-
 class addRecall extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
 
-  componentDidMount(){
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+ }
 
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
+
+    // update localStorage
+    localStorage.setItem(key, value);
+  }
+
+  addItem() {
+    // create a new item
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    // copy current list of items
+    const list = [...this.state.list];
+
+    // add the new item to the list
+    list.push(newItem);
+
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+
+    // update localStorage
+    localStorage.setItem("list", JSON.stringify(list));
+    localStorage.setItem("newItem", "");
+  }
+
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list];
+    // filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+
+    // update localStorage
+    localStorage.setItem("list", JSON.stringify(updatedList));
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
   }
 
   render() {
     return (
-        <div className="inputRecall">
-            
-            
+      <div className="addRecall">
+        
+        <h1 className="addRecall-title">Add new recalled vehicle</h1>
+        
+        <div
+          style={{
+            padding: 50,
+            textAlign: "left",
+            maxWidth: 500,
+            margin: "auto"
+          }}
+        >
+          Vehicle details
+          <br />
+          <input
+            type="text"
+            placeholder="Make"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Model"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Year"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            onClick={() => this.addItem()}
+            disabled={!this.state.newItem.length}
+          >
+          Save
+          </button>
+          <br /> <br />
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>
+                    Remove
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
         </div>
+      </div>
     );
   }
 }
