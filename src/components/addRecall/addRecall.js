5f66dcac633d41d27f46d34a7ccce8f759adb947
Kevin Horng <tkevin.horng@gmail.com>
2018-09-02 09:52:57 +1000

    merging with origin and fixing conflicts

--- src/components/addRecall/addRecall.js
+++ src/components/addRecall/addRecall.js
@@ -76,10 +76,6 @@ class addRecall extends Component {
           value = JSON.parse(value);
           this.setState({ [key]: value });
         } catch (e) {
-<<<<<<< HEAD
-          // handle empty string
-=======
->>>>>>> origin
           this.setState({ [key]: value });
         }
       }
@@ -89,13 +85,7 @@ class addRecall extends Component {
   render() {
     return (
       <div className="addRecall">
-<<<<<<< HEAD
-        
-        <h1 className="addRecall-title">Add new recalled vehicle</h1>
-        
-=======
           <h1 className="addRecall-title">Add Recall</h1>
->>>>>>> origin
         <div
           style={{
             padding: 50,
@@ -105,46 +95,6 @@ class addRecall extends Component {
           }}
         >
           Vehicle details
-<<<<<<< HEAD
-          <br />
-          <input
-            type="text"
-            placeholder="Make"
-            value={this.state.newItem}
-            onChange={e => this.updateInput("newItem", e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Model"
-            value={this.state.newItem}
-            onChange={e => this.updateInput("newItem", e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Year"
-            value={this.state.newItem}
-            onChange={e => this.updateInput("newItem", e.target.value)}
-          />
-          <button
-            onClick={() => this.addItem()}
-            disabled={!this.state.newItem.length}
-          >
-          Save
-          </button>
-          <br /> <br />
-          <ul>
-            {this.state.list.map(item => {
-              return (
-                <li key={item.id}>
-                  {item.value}
-                  <button onClick={() => this.deleteItem(item.id)}>
-                    Remove
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-=======
           <br /><br />
           <input
             type="text"
@@ -182,7 +132,6 @@ class addRecall extends Component {
           Save
           </button>
           </Link>
->>>>>>> origin
         </div>
       </div>
     );
