7e72b7164a584875f2287576297b89e4fc5ec9ae
malduba <41451295+malduba@users.noreply.github.com>
2018-09-23 21:40:17 +1000

    Updating to provide ability to view/add notes
    Updating to provide ability to view/add notes

--- src/components/detail/notes.js
+++ src/components/detail/notes.js
@@ -3,9 +3,37 @@ import React from 'react';
 export default function Notes(props) {
     return (
         <div className="detail-notes">
-            <div className="alert alert-warning">Currently No Notes</div>
-            <h2>Latest Recall Notes</h2><br/>
-            <button className="btn btn-primary disabled">New Note</button>
-        </div>
+            <table className="table notes-table">
+                <thead id="thead">
+                    <tr>
+                        <th scope="col"> </th>
+                        <th scope="col">Recorded Date</th>
+                        <th scope="col">Recorded By</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody id="tbody">
+                    <tr>
+                        <td></td>
+                        <td>04-05-2018</td>
+                        <td>Rashmi Nandakumar</td>
+                        <td>Sample test here. Sample text here. Sample text here. Sample text here. Sample text here. Sample text here. Sample text here. Sample text here. </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>08-06-2018</td>
+                        <td>Lara Cottrell</td>
+                        <td>Sample test here. Sample text here. Sample text here. Sample text here. Sample test here. Sample text here. Sample text here. Sample text here. </td>
+                    </tr>
+                    <tr>
+                        <td>save delete</td>
+                        <td>08-06-2018</td>
+                        <td>Lara Cottrell</td>
+                        <td><input type="text" size="45" name="comments" id="comments"/></td>
+                    </tr>
+
+                </tbody>
+            </table>
+    </div>
     );
 }
