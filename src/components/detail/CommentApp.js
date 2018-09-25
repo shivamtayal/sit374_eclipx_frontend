15691c0b1c2dab554e9551e90c9c1e98bef97618
nicholasshenjh <42369162+nicholasshenjh@users.noreply.github.com>
2018-09-25 18:18:48 +1000

    Add files via upload

---
+++ src/components/detail/CommentApp.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import CommentInput from './CommentInput'
+import CommentList from './CommentList'
+
+class CommentApp extends Component {
+  constructor () {
+    super()
+    this.state = {
+      comments: []
+    }
+  }
+
+  handleSubmitComment (comment) {
+    if (!comment) return
+    if (!comment.username) return alert('Please input username')
+    if (!comment.content) return alert('Please input notes')
+    this.state.comments.push(comment)
+    this.setState({
+      comments: this.state.comments
+    })
+  }
+
+  render() {
+    return (
+      <div className='wrapper'>
+        <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
+        <CommentList comments={this.state.comments}/>
+      </div>
+    )
+  }
+}
+
+export default CommentApp
