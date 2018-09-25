7105ccfe6c1f351b10882425033fd161af8326af
nicholasshenjh <42369162+nicholasshenjh@users.noreply.github.com>
2018-09-25 18:19:53 +1000

    Add files via upload

--- src/components/detail/notes.js
+++ src/components/detail/notes.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
+import './notes.css'
 
 export default function Notes(props) {
-    return (
-        <div className="detail-notes">
-            <div className="alert alert-warning">Currently No Notes</div>
-            <h2>Latest Recall Notes</h2><br/>
-            <button className="btn btn-primary disabled">New Note</button>
-        </div>
-    );
-}
+  return (
+      <div className="detail-notes">
+          <div className="alert alert-warning">Currently No Notes</div>
+          <h2>Latest Recall Notes</h2><br/>
+          <button className="btn btn-primary disabled">New Note</button>
+      </div>
+  );
+  ReactDOM.render(
+    <CommentApp />,
+    document.getElementById('root')
+  )
+  }
+
+
