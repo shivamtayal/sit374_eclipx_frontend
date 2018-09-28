a82263d2f8a5c396c9a3da4f1bb1188eeef0692c
Kevin Horng <tkevin.horng@gmail.com>
2018-09-28 13:32:28 +1000

    saving

--- src/components/recalls/recallSlim.js
+++ src/components/recalls/recallSlim.js
@@ -8,37 +8,37 @@ class RecallSlim extends Component {
     }
 
     render(){
-        return (
+        return (     
             <Link className="recall-item-link" to={`/recall/${this.props.id}`}>
             <li className="list-group-item">
                 <div className="row">
                     {/*this.props.automatic ? <i className="fas fa-exclamation-circle fa-1x automatic"></i> : null */}
                     <div className="col result-group">
-                        VIN:<br/>
+                        VIN<br/>
                         <span className="badge badge-dark">{this.props.vin ? this.props.vin : 'N/A'}</span>
                     </div>
                     <div className="col result-group">
-                        Manufacturer:<br/>
+                        Manufacurer<br/>
                         <span className="badge badge-dark">{this.props.manufacturer ? this.props.manufacturer : 'N/A'}</span>
                     </div>
                     <div className="col result-group">
-                        Make:<br/>
+                        Make<br/>
                         <span className="badge badge-dark">{this.props.make ? this.props.make : 'N/A'}</span>
                     </div>
                     <div className="col result-group">
-                        Year:<br/>
+                        Year<br/>
                         <span className="badge badge-dark">{this.props.year ? this.props.year : 'N/A'}</span>
                     </div>
                     <div className="col result-group">
-                        Registration:<br/>
+                        Registration<br/>
                         <span className="badge badge-dark">{this.props.registration ? this.props.registration : 'N/A'}</span>
                     </div>
                     <div className="col result-group">
-                        Recalls:<br/>
+                        Recalls<br/>
                         <span className="badge badge-dark">{this.props.recallCount ? this.props.recallCount : '0'}</span>
                     </div>
                     <div className="col result-group">
-                        Active:<br/>
+                        Active<br/>
                         <span className="badge badge-dark">{this.props.activeRecall ? this.props.activeRecall : 'N/A'}</span>
                     </div>
                 </div>
